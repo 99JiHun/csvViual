ccbaaeac4217bdc730bfd1f925656dcaf9c6848b
JiHun Choe <94276325+99JiHun@users.noreply.github.com>
2024-09-01 01:01:06 +0900

    visual update v0.01

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,9 @@ const dropArea = document.getElementById('drop-area');
 const fileInput = document.getElementById('filename');
 const clickArea = document.getElementById('click-area');
 const resetArea = document.getElementById('reset-area');
+const canvas = document.getElementById('myChart');
 
-let allData = []; // 모든 파일 데이터 저장 배열
-let hearders = []; // 모든 csv 파일에서 발견한 고유한 헤더
+let allData = [];
 
 dropArea.addEventListener('dragover', (e)=>{
     e.preventDefault();
@@ -35,10 +35,18 @@ resetArea.addEventListener('click',(e)=>{
     tables.forEach(table => {
         dropArea.removeChild(table);
     });
+
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.Width, canvas.height);
+
+    if(window.chartInstance){
+        window.chartInstance.destroy();
+    }
+
+
     fileInput.value = '';
     allData.splice(0,allData.length);
-    hearders.splice(0,hearders.length);
-
+    console.log("data and visual reset.");
     e.preventDefault();
 })
 
@@ -55,6 +63,7 @@ fileInput.addEventListener('change', (e)=>{
 function parseCSV(csvText){
     const rows = csvText.trim().split("\n").map(row => row.split(","));
     const headers = rows[0].map(header => header.trim());
+    console.log(headers, rows);
     const data = rows.slice(1).map(row =>{
         return row.map((cell, index)=>{
             const trimmedCell = cell.trim();
@@ -69,52 +78,33 @@ function handleFiles(files){
         const reader = new FileReader();
         reader.onload = function(event){
             const csvText = event.target.result;
-            // const {headers, data} = parseCSV(csvText);
-
-            const rows = csvText.split("\n");
-            const fileData = [];
-    
-            rows.forEach((row, rowIndex)=>{
-                const cols = row.split(",").map(col => col.trim());
-                const hasValidData = cols.some(col => col.length > 0);
-                if(rowIndex === 0){
-                    cols.forEach(col=>{
-                        if(!hearders.includes(col.trim())){
-                            hearders.push(col.trim());
-                        }
-                    });
-                } else if(hasValidData){
-                    const rowData = {};
-                    cols.forEach((col,colIndex)=>{
-                        rowData[hearders[colIndex]] = col.trim();
-                    });
-                    fileData.push(rowData);
-                }
-            });
-            allData = allData.concat(fileData);
-            updateTable();
-            generateChartData();
+            const {headers, data} = parseCSV(csvText);
+            
+            allData = allData.concat(data);
+            
+            updateTable(headers, allData);
+            generateChartData(headers, allData);
         };
         reader.readAsText(file);
     }
 }
 
-function updateTable(){
+function updateTable(headers, data){
     const table = document.createElement("table");
     const headerRow = document.createElement("tr");
 
-    hearders.forEach(header =>{
+    headers.forEach(header =>{
         const th = document.createElement("th");
         th.textContent = header;
         headerRow.appendChild(th);
     });
     table.appendChild(headerRow);
 
-    allData.forEach(rowData =>{
+    data.forEach(rowData =>{
         const tr = document.createElement("tr");
-        hearders.forEach(header =>{
+        rowData.forEach(cell =>{
             const td = document.createElement("td");
-            td.textContent = rowData[header] || "n";
+            td.textContent = cell || "n";
             tr.appendChild(td);
         });
         table.appendChild(tr);
@@ -127,8 +117,13 @@ function updateTable(){
 }
 
 function createChart(labels, datasets){
-    const ctx = document.getElementById('myChart').getContext('2d');
-    new Chart(ctx, {
+    const ctx = canvas.getContext('2d');
+
+    if(window.chartInstance){
+        window.chartInstance.destroy();
+    }
+
+    window.chartInstance = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: labels,
@@ -144,14 +139,13 @@ function createChart(labels, datasets){
     });
 }
 
-function generateChartData(){
-    const labels = allData.map(row => row['A']);
-    const datasetkeys = ['B','C','D','E'];
-
-    const datasets = datasetkeys.map(key=>{
+function generateChartData(headers, data){
+    const labels = headers;
+    const datasets = data.slice(0).map((header, index)=>{
         return{
-            label : key,
-            data : allData.map(row => parseFloat(row[key])),
+            label : headers[index],
+            // data : data.map(row => parseFloat(row[index])),
+            data : data.map(row => parseFloat(row[index])),
             backgroundColor: getRandomColor(),
             borderColor: getRandomColor(),
             borderWidth: 1,
